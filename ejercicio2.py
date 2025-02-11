5d4044b24b717abf6ed5ba329ec0c39d6aeaeec3
Arnold-Js <79930202+Arnold-Js@users.noreply.github.com>
2025-02-11 09:13:00 -0600

    2 ejercicio-Declaracion de variables + suma

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+#Ejercicio 2. Declaracion de variables + suma
+nombre = "Arnol Jesus"
+edad = 25
+altura = 1.60
+
+#imprimir datos
+print("Nombre: ",nombre)
+print("Edad: ",edad)
+print("Altura: ",altura)
+
+#Operacion simple
+edad_en_5_a= edad +5
+print("En 5 años tendre ",edad_en_5_a, "años")
