9db7c3af44ce1116e87b5b1bc741376061b3c972
Arnold-Js <79930202+Arnold-Js@users.noreply.github.com>
2025-02-12 19:41:49 -0600

    11 ejercicio numero positivo o negativo

---
+++ ejercicio11.py
@@ -0,0 +1,9 @@
+#ejercicio11: Numero Positivo o Negativo
+num = int(input("ingresa un numero: "))
+if num > 0:
+    print(f"{num} es positivo.")
+elif num<0:
+    print(f"{num} es negativo.")
+else:
+    print("el numero es cero.")
+    
