6f0070f363ebcdcb6dd2f223fa7a484152e8884c
Arnold-Js <79930202+Arnold-Js@users.noreply.github.com>
2025-02-12 19:01:18 -0600

    4 ejercicio-Entrada de datos

---
+++ ejercicio4.py
@@ -0,0 +1,10 @@
+#ejercicio 4-Entrada de datos
+nombreCompleto= str(input("ingresa tu Nombre Completo: "))
+carrera=str(input("igresa tu Carrera: "))
+semestre=int(input("ingresa tu Semestre:"))
+materia=str(input("ingresa tu Materia: "))
+
+print("Nombre Completo: ",nombreCompleto)
+print("Carrera: ",carrera)
+print("Semestre: ",semestre)
+print("Materia: ",materia)
