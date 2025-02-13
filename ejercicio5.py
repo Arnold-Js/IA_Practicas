2e48288de580db443a6a642cee652b736d41aaad
Arnold-Js <79930202+Arnold-Js@users.noreply.github.com>
2025-02-12 19:26:46 -0600

    5 ejercicio-Entrada de datos Suma

---
+++ ejercicio5.py
@@ -0,0 +1,7 @@
+#ejercicio 6 multiplicacion y resta
+num1 = float(input("ingresa el primer numero:\n"))
+num2 = float(input("ingresa el segundo numero:\n"))
+rest= num1 - num2
+mult = num1* num2
+print(f"\nLa resta de {num1} y {num2} es: {rest}")
+print(f"\nLa multiplicaion de {num1} y {num2} es: {mult}\nPrograma elaborado por Arnol Jesus")
